db75b6d8d274545982963ba80d83252a8e98cc57
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-03 01:24:52 +0530

    Added:code to delete contact details from Address Book

---
+++ src/com/blz/prtcproblem/AddressBookMain.java
@@ -0,0 +1,47 @@
+package com.blz.prtcproblem;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+
+		entryPickUp();
+	}
+
+	static void entryPickUp() {
+		AddressBook addressBook = new AddressBook();
+		Scanner sc = new Scanner(System.in);
+		int flag = 1;
+		while (flag == 1) {
+			System.out.println("Welcome to address book program ");
+			System.out.println("Enter choice \n1. AddContact \n2. Edit \n3. Delete \n4. Exit ");
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				addressBook.addContact();
+				break;
+			case 2:
+				if (AddressBook.lst.isEmpty()) {
+					System.out.println(" Address book is empty ");
+					break;
+				}
+				addressBook.editContact();
+				break;
+			case 3:
+				if (AddressBook.lst.isEmpty()) {
+					System.out.println(" Address book is empty ");
+					break;
+				}
+				addressBook.deleteContact();
+				break;
+			case 4:
+				flag = 0;
+				System.out.println("Exit");
+				break;
+			default:
+				System.out.println("Invalid Choice");
+			}
+		}
+	}
+}
