654f4a963d29f1dcbb176e5f065511c4aea15ed6
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-03 17:26:52 +0530

    Added:code to add new contact into db.

---
+++ lib/src/main/java/AddressBook/AddressBookService.java
@@ -0,0 +1,73 @@
+package AddressBook;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+public class AddressBookService {
+
+	public enum IOService {
+		DB_IO, FILE_IO
+	}
+
+	private List<PersonData> addressBookList;
+	private static AddressBookDBService addressBookDBService;
+
+	public AddressBookService() {
+		addressBookDBService = AddressBookDBService.getInstance();
+	}
+
+	public List<PersonData> readAddressBookData(IOService ioService) throws AddressBookException {
+		if (ioService.equals(IOService.DB_IO))
+			return this.addressBookList = addressBookDBService.readData();
+		return this.addressBookList;
+	}
+
+	public void updateContactAddress(String firstname, String address) throws AddressBookException {
+		int result = addressBookDBService.updateAddressBookData(firstname, address);
+		if (result == 0)
+			throw new AddressBookException("Address book update failed",
+					AddressBookException.ExceptionType.DatabaseException);
+		PersonData addressBookData = this.getAddressBookData(firstname);
+		if (addressBookData != null)
+			addressBookData.address = address;
+	}
+
+	private PersonData getAddressBookData(String firstname) {
+		return this.addressBookList.stream().filter(addressBookItem -> addressBookItem.firstName.equals(firstname))
+				.findFirst().orElse(null);
+	}
+
+	public boolean checkAddressBookInSyncWithDatabase(String firstname) throws AddressBookException {
+		try {
+			List<PersonData> addressBookData = addressBookDBService.getAddressBookData(firstname);
+			return addressBookData.get(0).equals(getAddressBookData(firstname));
+		} catch (AddressBookException addressBookException) {
+			throw new AddressBookException("Cannot execute query",
+					AddressBookException.ExceptionType.DatabaseException);
+		}
+	}
+
+	public List<PersonData> readEmployeePayrollData(IOService ioService, String start, String end)
+			throws AddressBookException {
+		try {
+			LocalDate startLocalDate = LocalDate.parse(start);
+			LocalDate endLocalDate = LocalDate.parse(end);
+			if (ioService.equals(IOService.DB_IO))
+				return addressBookDBService.readData(startLocalDate, endLocalDate);
+			return this.addressBookList;
+		} catch (AddressBookException e) {
+			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.DatabaseException);
+		}
+	}
+
+	public int readEmployeePayrollData(String function, String city) throws AddressBookException {
+		return addressBookDBService.readDataPayroll(function, city);
+	}
+	
+	public void addNewContact(String firstName, String lastName, String address, String city, String state, int zip,
+			int phoneNumber, String email) throws AddressBookException, SQLException {
+		addressBookList.add(
+				addressBookDBService.addNewContact(firstName, lastName, address, city, state, zip, phoneNumber, email));
+	}
+}
---
+++ lib/src/test/java/AddressBook/AddressBookServiceTest.java
@@ -0,0 +1,54 @@
+package AddressBook;
+
+import java.sql.SQLException;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import AddressBook.AddressBookService.IOService;
+
+public class AddressBookServiceTest {
+
+	private static AddressBookService addressBook;
+
+	@BeforeClass
+	public static void createServiceObj() {
+		addressBook = new AddressBookService();
+		System.out.println("Welcome to the Address Book System.. ");
+	}
+
+	@Test
+	public void givenAddressBook_WhenRetrieved_ShouldMatchContactsCount() throws AddressBookException, SQLException {
+		List<PersonData> personData = addressBook.readAddressBookData(IOService.DB_IO);
+		Assert.assertEquals(4, personData.size());
+	}
+
+	@Test
+	public void givenAddressBook_WhenUpdate_ShouldSyncWithDB() throws AddressBookException {
+		List<PersonData> personDetails = addressBook.readAddressBookData(IOService.DB_IO);
+		addressBook.updateContactAddress("Pallavi", "EcoVillage");
+		boolean result = addressBook.checkAddressBookInSyncWithDatabase("Pallavi");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void givenAddressBook_WhenRetrieved_ShouldMatchAddressBookCountInGivenRange() throws AddressBookException {
+		List<PersonData> personDetails = addressBook.readEmployeePayrollData(IOService.DB_IO, "2020-11-01",
+				"2020-11-22");
+		Assert.assertEquals(2, personDetails.size());
+	}
+
+	@Test
+	public void givenAddresBook_WhenRetrieved_wShouldReturnTotalNoOfCity() throws AddressBookException {
+		Assert.assertEquals(1, addressBook.readEmployeePayrollData("Count", "Varanasi"));
+	}
+
+	@Test
+	public void givenAddresBook_WhenAdded_ShouldSyncWithDB() throws AddressBookException, SQLException {
+		addressBook.readAddressBookData(IOService.DB_IO);
+		addressBook.addNewContact("Anika", "Srivastava", "Btm", "Bangalore", "KA", 561234, 985434211,
+				"ani@gmail.com");
+		boolean result = addressBook.checkAddressBookInSyncWithDatabase("Ani");
+		Assert.assertTrue(result);
+	}
+}
